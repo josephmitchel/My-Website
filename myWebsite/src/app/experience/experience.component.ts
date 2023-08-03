b39fda1ff600362530f8754a413a38a677f440c7
Joseph Mitchell <jmit@bu.edu>
2023-08-03 14:11:52 -0400

    added some fun hover animations

--- myWebsite/src/app/experience/experience.component.ts
+++ myWebsite/src/app/experience/experience.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fade } from '../animations';
 
 @Component({
   selector: 'tab-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.css'],
+  animations: [fade]
 })
 export class ExperienceTab {
   defaultElevation = 0;
