275e7ed196768e7331d31c4283cba70982e8606b
Joseph Mitchell <jmit@bu.edu>
2024-08-12 14:28:54 -0400

    Fixed the content element not centering properly

--- myWebsite/src/app/experience/experience.component.ts
+++ myWebsite/src/app/experience/experience.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { fade } from '../animations';
+import { fade, fade2 } from '../animations';
 
 @Component({
   selector: 'tab-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css'],
-  animations: [fade]
+  animations: [fade, fade2]
 })
 export class ExperienceTab {
   defaultElevation = 0;
